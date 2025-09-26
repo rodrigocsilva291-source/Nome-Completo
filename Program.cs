0ef34fba4796d7bf5e14fc6875b12475d12cf94d
Rodrigo <rodrigocsilva291@gmail.com>
2025-09-26 20:03:42 -0300

    atividade realizada

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string nome,  sobrenome; 
+Console.Write("Digite seu primeiro nome");
+nome = Console.ReadLine();
+Console.Write("Digite seu sobrenome:");
+sobrenome = Console.ReadLine();
+ 
+Console.WriteLine($"{nome} {sobrenome}");
+Console.WriteLine($"{sobrenome.ToUpper()},  {nome}");
